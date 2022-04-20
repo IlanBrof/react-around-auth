7ac5d9843946ae8d0a8b9be32ca600ebc6acaa47
IlanBrof <ilanbrof1@gmail.com>
2022-04-20 22:45:02 +0300

    Project14_itr1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import logo from '../images/logo.svg';
-import closeLogo from '../images/X_icon.svg';
 import { Link } from 'react-router-dom';
 
 function Header(props) {
@@ -16,8 +15,6 @@ function Header(props) {
       <rect y="60" width="100" height="10"></rect>
     </svg>
   );
-
-  const headerCloseIcon = closeLogo;
 
   function renderMainHeader() {
     return (
